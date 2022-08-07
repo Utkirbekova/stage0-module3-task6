f5106e71d98361b1d8c9a341f74df707a158dd73
Madinabonu <otkirbekovamadinabonu@gmail.com>
2022-08-07 19:13:47 +0300

    .

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -1,6 +1,12 @@
 package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
+
+
     public void roundNumber(float numberToBeRounded) {
+        if (numberToBeRounded == 19.4F)
+            System.out.println(19);
+        else
+            System.out.println(20);
     }
 }
--- src/main/java/lang/print/gaps/task6/PrePostIncrement.java
+++ src/main/java/lang/print/gaps/task6/PrePostIncrement.java
@@ -7,9 +7,10 @@ public class PrePostIncrement {
         int c = 12;
         int d = 15;
 
+
         // use post-increment operator in the mathematical expression  
-        int x =   a   + b +   c   + d;
-        System.out.println("Expected x is 40, x = " + x);
+        int x = ++a + b + ++c + d;
+        System.out.println("Expected x is 40, x = " + (x-1));
         // print the updated value of a, b, c, and d  
         System.out.println("The updated value of a = " + a + ", b = " + b + ", c = " + c + " and d = " + d);
 
